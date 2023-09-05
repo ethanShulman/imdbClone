57fa24f88c0c3ee56c5ec072171086ca823cefd5
COR Coding <corcoding@coding-14.attlocal.net>
2023-09-05 01:31:16 -0400

    Adds bcrypt hashing to passwords.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const { user } = require('./models');
 const path = require('path');
 const bcrypt = require('bcrypt');
-const plaintextPassword = 'mypassword';
 const saltRounds = 10;
 
 app.set('view engine', 'ejs');
@@ -12,33 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-// app.use('/static', express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/public')))
-
-
-// bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('Hashed password:', hash);
-// });
-
-const userEnteredPassword = 'mypassword';
-const storedHashedPassword = '...'; // this is the password that is stored in the database
-
-// bcrypt.compare(userEnteredPassword, storedHashedPassword, (err, result) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   if (result) {
-//     console.log('Passwords match!');
-//   } else {
-//     console.log('Passwords do not match.');
-//   }
-// });
 
 
 app.post('/log_in', async (req, res) => {
@@ -69,29 +42,32 @@ app.put('/', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { name, email, password, passwordCheck } = req.body;
-    const existingUser = await user.findOne({ where: { email } })
+    const existingUser = await user.findOne({ where: { email } });
 
-    if(existingUser){
-        res.status(400).json({
+    if (existingUser) {
+        return res.status(400).json({
             error: 'Email is already in use'
-        })
-    }
-
-    if(req.body.password !== req.body.passwordCheck){
-        res.status(400).json({
-            error:'passwords must match'
         });
     }
 
-    await user.create({
-    name,
-    email,
-    password
-})
-    
-    
-    res.render('register');
+    if (password !== passwordCheck) {
+        return res.status(400).json({
+            error: 'Passwords must match'
+        });
+    }
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        user.create({
+                name,
+                email,
+                password: hash 
+            })
+            .then(() => {
+                res.render('register');
+            })
+    });
 });
+
 
 app.delete('/', (req, res) => {
     
@@ -99,4 +75,4 @@ app.delete('/', (req, res) => {
 
 app.listen(3001, () =>{
     console.log('Server is running on port 3001')
-});
+})
